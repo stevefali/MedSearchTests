77dcc2a2c684794efe46e77784f39c25db1575dd
stevefali <139910217+stevefali@users.noreply.github.com>
2024-08-06 19:25:28 -0700

    Add new route for getting interactions from a list of medications.

--- routes/boxedWarningRoutes.js
+++ routes/boxedWarningRoutes.js
@@ -10,9 +10,9 @@ router.get("/", async (req, res) => {
   try {
     const fdaResponse = await axios.get(getBoxedWarningEndpoint(medicine));
 
-    console.log(fdaResponse.data.results[0].drug_interactions[0]);
+    const resultObj = fdaResponse.data.results[0];
 
-    const resultString = fdaResponse.data.results[0].drug_interactions[0];
+    let resultString = resultObj.drug_interactions[0];
 
     const matches = getInteractorMatch(resultString, interactor);
 
@@ -23,7 +23,61 @@ router.get("/", async (req, res) => {
     });
   } catch (err) {
     console.log(err);
+    res.json({ error: `No results found for medicine named ${medicine}.` });
   }
+});
+
+router.get("/mult", async (req, res) => {
+  const medicines = ["tacrolimus", "amoxicillin", "ozempic", "ursodiol"];
+  const { interactor } = req.query;
+
+  const fdaCalls = [];
+  for (const medicine of medicines) {
+    fdaCalls.push(async () => {
+      try {
+        return {
+          medicine: medicine,
+          resp: await axios.get(getBoxedWarningEndpoint(medicine)),
+        };
+      } catch (error) {
+        return {
+          medicine: medicine,
+          resp: false,
+        };
+      }
+    });
+  }
+
+  const fdaResponses = await Promise.allSettled(
+    fdaCalls.map((fdaCall) => fdaCall())
+  );
+
+  const interactionsResponse = [];
+  const noneFound = `No interactions with ${interactor} found.`;
+
+  for (const fdaResponse of fdaResponses) {
+    let medName = fdaResponse.value.medicine;
+    if (fdaResponse.value.resp) {
+      const matches = getInteractorMatch(
+        fdaResponse.value.resp.data.results[0].drug_interactions[0],
+        interactor
+      );
+      interactionsResponse.push({
+        medicine: medName,
+        matches: matches.length > 0 ? matches : noneFound,
+      });
+    } else {
+      interactionsResponse.push({
+        medicine: medName,
+        matches: noneFound,
+      });
+    }
+  }
+
+  res.json({
+    interactionsResponse,
+    disclaimer: "Disclaimer here",
+  });
 });
 
 module.exports = router;
